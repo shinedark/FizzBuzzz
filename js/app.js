f9a645935cff7b83cf51da6f8dd77392af4fa1d1
shinedark <Shine@camilos-MacBook-Pro.local>
2016-08-08 13:01:09 -0500

    new version

--- js/app.js
+++ js/app.js
@@ -1,12 +1,45 @@
 $(document).ready(function() {
 
-	for (var i = 1; i < 101; i++) {
+	// respond to submit
+	$(".userInput > button").on("click", function(evt) {
+		evt.preventDefault();
+
+		clearFizz();
+
+		var userInput = $(".userInput > input").val();
+
+		var userInputNumber = Number(userInput);
+
+		if (isNaN(userInputNumber)) {
+			alert('you had one job... this is not a valid number');
+			return;
+		}
+
+		// test for decimal 
+		if (userInputNumber % 1 !== 0) {
+			alert('you had one job... decimals are not allowed');
+			return;
+		}
+
+		if (userInputNumber > 100000) {
+			alert('you had one job... are you kidding me? Number too big! have fun with the long wait');
+			// return;
+		}
+
+		doFizzBuzz(userInputNumber);
+	});
+});
+
+// function to run fizzbuzz
+// will call: `getFizzBuzz` and `appendToFizz`
+// should accept a argument to determine how high to loop
+
+function doFizzBuzz(numberToCountTo) {
+	for (var i = 1; i <= numberToCountTo; i++) {
   		var output = getFizzBuzz(i);
 		appendToFizz(output);
 	}
-
-})
-	
+}
 
 function getFizzBuzz(numberToTest) {
 	
@@ -22,10 +55,15 @@ function getFizzBuzz(numberToTest) {
 	else {
 		return numberToTest;
 	}
-	
 }
 
 function appendToFizz(content) {
-	$('.fizz').append('<p>' + content + '</p>');
+  	$('.fizz').append('<p>' + content + '</p>');
+ }
+
+function clearFizz() {
+	$('.fizz').empty();
 }
+
+  
 
